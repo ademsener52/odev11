0c4aff5436bc4778d4c2ecac9a8f3a8f5876667c
Adem ŞENER <adem.sener@unye.bel>
2021-12-30 10:53:17 +0300

    Ödev 11

---
+++ odev11.sql
@@ -0,0 +1,23 @@
+(select first_name from actor)
+union
+(select first_name from customer);  --1
+
+(select first_name from actor)
+intersect
+(select first_name from customer);  --2
+
+(select first_name from actor)
+except
+(select first_name from customer);  --3
+
+(select first_name from actor)
+union all
+(select first_name from customer);  --4.1
+
+(select first_name from actor)
+intersect all
+(select first_name from customer);  --4.2
+
+(select first_name from actor)
+except all
+(select first_name from customer);  --4.3
